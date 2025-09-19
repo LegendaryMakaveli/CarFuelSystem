fddc58685074c5f5a16dc9117e527fcaabcf9263
LengendaryMakaveli <adeyemoakorede93@gmail.com>
2025-09-19 21:50:08 +0100

    test function that turn the car on and off done

---
+++ Test/testCarFuelSystem.test.js
@@ -0,0 +1,32 @@
+const {ignition, powerSwith, carTankCapacity} = require("../SourceCode/carSystem");
+
+describe("Test that my car can be turned on", () => {
+    test("test that the ignition is off by default", () => {
+        expect(ignition()).toBe(false);
+    });
+
+    test("test that my car can be turn on", () => {
+        expect(ignition()).toBe(false);
+        powerSwith();
+        expect(ignition()).toBe(false);
+        powerSwith();
+    });
+
+    test("test that my car can be turned off", () => {
+        powerSwith();
+        powerSwith();
+        expect(ignition()).toBe(false);
+    });
+});
+
+describe("Test my car tank and its capacity", () => {
+    test("test that my car tank is not empty", () => {
+        expect(carTankCapacity()).toBe(50);
+    })
+
+    test("test thst my car dont start if the fuel is empty", () => {
+        let carFuel = 0
+        powerSwith()
+        expect(ignition()).toBe (false)
+    })
+})
